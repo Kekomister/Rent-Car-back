9cf6f046108b88b58e8ea3a9bb6b56a93b13c4a6
Kekomister <jeremyg1999@example.com>
2024-07-26 16:23:42 -0300

    port

--- config/conection.ts
+++ config/conection.ts
@@ -20,12 +20,12 @@
 //   };
 
 
-const config = {
-  host: "localhost",
-  user: "rentcars",
-  password: "",
-  database: 'rentcars',
-  port: 3306
-}
+// const config = {
+//   host: "localhost",
+//   user: "rentcars",
+//   password: "",
+//   database: 'rentcars',
+//   port: 3306
+// }
 
-export default config;
+// export default config;
